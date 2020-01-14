4b87da9348e984934b4def58637382564d7f026c
alnhdgrlla <t.yoshi7227@gmail.com>
2020-01-14 18:16:37 +0900

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :artists
+  resources :albums
+    resources :reviews, only: [ :new, :create, :show, :destroy ] do
+  end
+  resources :songs
+  resources :genre, only: [:index]
 end
