1dff92d4e202870b68c385a358360546dcb5669a
alnhdgrlla <t.yoshi7227@gmail.com>
2020-01-14 17:47:22 +0900

    fixed models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,6 @@
 class Album < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  has_many :songs, dependent: :destroy
   belongs_to :user
+  belongs_to :genre
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :artists
+  has_many :albums
+  has_many :songs
+  has_many :reviews
 end
